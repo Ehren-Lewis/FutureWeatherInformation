5a01d272334c7af00f183f99ee212438b39f160f
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-08-17 21:52:01 -0500

    Working on pulling the 5 day forecast. Set the information from the first APi call into the DOM

--- Assets/scripts/index.js
+++ Assets/scripts/index.js
@@ -16,21 +16,13 @@ let geoCall = "http://api.openweathermap.org/geo/1.0/direct?"
 // then create the query param, q=citbane&appid=key;
 
 // q={city name}&appid={API key}"
-const city = "Fort+Worth";
+const city = "Fort Worth";
 
-const query = `q=${city}&appid=dbd4b78b875c9f3d499f25008225a8e6`;
+const queryCity = city.replaceAll(" ", "+");
+const query = `q=${queryCity}&appid=dbd4b78b875c9f3d499f25008225a8e6`;
+
 
 const fullGeoCall = geoCall + query;
-
-// fetch(fullGeoCall).then(
-//     (value) => {
-//         console.log(value);
-//     }, (error) => {
-//         console.log(error);
-//     }
-// )
-
-// $("#currentCity").load(fullGeoCall);
 
 $.ajax({
     url: fullGeoCall,
@@ -42,10 +34,35 @@ $.ajax({
     return [lat, lon];
 }).then( (coords) => {
         $.ajax({
-            url: `${url}lat=${coords[0]}&lon=${coords[1]}&appid=${key}`,
+            url: `${url}lat=${coords[0]}&lon=${coords[1]}&appid=${key}&units=imperial`,
             method: "GET"
         }).then( (value) => {
-            console.log(value);
+            
+            const name = value.name;
+            const temp = value.main.temp;
+            const humidity = value.main.humidity;
+            const windSpeed = value.wind.speed;
+            const visibility = value.weather[0].main;
+            const description = value.weather[0].description;
+            const icon = value.weather[0].icon;
+            const weatherImg = $(`<img src="http://openweathermap.org/img/wn/${icon}.png">`);
+            $("#forecast").append(weatherImg);
+            $(".cityName").text(name);
+            $(".icon").append(weatherImg);
+            $(".temperature").text(`Temperature: ${temp}`);
+            $(".windSpeed").text(`Wind speed: ${windSpeed} MPH`);
+            $(".humidity").text(`Humidity: ${humidity} %`);
+            return [value.coord.lat, value.coord.lon];
+            //  Add UV Call
+        }).then ( (coords) => {
+            console.log(coords);
+            $.ajax({
+            // forecast call 
+            url: `http://api.openweathermap.org/data/2.5/forecast?lat=${coords[0]}&lon=${coords[1]}&appid=${key}&units=imperial`,
+            method: "GET"
+            }).then( (value ) => {
+                console.log(value);
+            })
         })
 
 }
@@ -53,3 +70,6 @@ $.ajax({
 ).fail( (error) => {
     console.log(error);
 })
+
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
